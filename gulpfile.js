b4e1495cea1d0a5ee50a5d7965d876b7fe86abe2
Ron Sogueco <ron@soguecode.io>
2018-03-05 23:52:46 -0500

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	coffee = require('gulp-coffee'),
+	connect = require('gulp-connect'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
+
+var coffeeSources = ['scripts/hello.coffee'],
+	jsSources = ['scripts/*.js'],
+	sassSources = ['styles/*.scss'],
+	htmlSources = ['**/*.html'],
+	outputDir = 'assets';
+
+gulp.task('copy', function() {
+	gulp.src('index.html')
+	.pipe(gulp.dest(outputDir))
+});
+
+gulp.task('sass', function() {
+	gulp.src(sassSources)
+	.pipe(sass({style: 'expanded'}))
+		.on('error', gutil.log)
+	.pipe(gulp.dest('assets'))
+	.pipe(connect.reload())
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(uglify())
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest(outputDir))
+	.pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch(sassSources, ['sass']);
+	gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('serve', function() {
+	connect.server({
+		root: '.',
+		livereload: true
+	})
+});
+
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'js', 'sass', 'serve', 'watch']);
